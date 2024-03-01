e1836620b25481cead7cec3216806922e52fc4fe
janikowski <161576033+janikowski@users.noreply.github.com>
2024-03-01 14:41:44 +0100

    add actor class

---
+++ MovieLibrary/src/main/java/org/example/models/Actor.java
@@ -0,0 +1,14 @@
+package org.example.models;
+
+public class Actor {
+
+    private String firstname;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+}
